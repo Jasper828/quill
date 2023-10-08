f14f72047757024ca82e9ea5cba2e76f51ab559e
Jasper828 <134510924+Jasper828@users.noreply.github.com>
2023-10-08 18:58:27 +0800

    Update pinecone.ts

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,21 +1,21 @@
-// import { PineconeClient } from '@pinecone-database/pinecone'
-
-// export const getPineconeClient = async () => {
-//   const client = new PineconeClient()
-
-//   await client.init({
-//     apiKey: process.env.PINECONE_API_KEY!,
-//     environment: 'gcp-starter',
-//   })
-
-//   return client
-// }
-import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeClient } from '@pinecone-database/pinecone'
 
 export const getPineconeClient = async () => {
-  const pinecone = new Pinecone({
+  const client = new PineconeClient()
+
+  await client.init({
     apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'gcp-starter', //check on app.pinecone.io
+    environment: 'gcp-starter',
   })
-  return pinecone;
-};
+
+  return client
+}
+// import { Pinecone } from '@pinecone-database/pinecone';
+
+// export const getPineconeClient = async () => {
+//   const pinecone = new Pinecone({
+//     apiKey: process.env.PINECONE_API_KEY!,
+//     environment: 'gcp-starter', //check on app.pinecone.io
+//   })
+//   return pinecone;
+// };
